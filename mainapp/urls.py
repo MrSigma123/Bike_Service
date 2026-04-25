06bb23a6a29859cf20703eba3805c9024ee23b08
MrSigma123 <karol.czolpinski@gmail.com>
2026-04-25 16:41:25 +0200

    Add urls for subpages

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('rowery/', views.rowery, name='rowery'),
+	path('zgloszenia/', views.zgloszenia, name='zgloszenia'),
+	path('czesci/', views.czesci, name='czesci'),
 ]
+
