b79dbb602dfebad11cdbab7effec1327646d1b29
MrSigma123 <karol.czolpinski@gmail.com>
2026-05-22 17:51:56 +0200

    Modify urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('rowery/', views.rowery, name='rowery'),
-	path('zgloszenia/', views.zgloszenia, name='zgloszenia'),
-	path('czesci/', views.czesci, name='czesci'),
-]
 
+    path('rowery/', views.rowery, name='rowery'),
+    path('rowery/dodaj/', views.dodaj_rower, name='dodaj_rower'),
+
+    path('zgloszenia/', views.zgloszenia, name='zgloszenia'),
+    path('zgloszenia/dodaj/', views.dodaj_zgloszenie, name='dodaj_zgloszenie'),
+
+    path('czesci/', views.czesci, name='czesci'),
+]
