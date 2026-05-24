c1561ba8febad08ae5ad7d274e8a0c9655b9ad1b
MrSigma123 <karol.czolpinski@gmail.com>
2026-05-25 00:19:26 +0200

    Test validated app version

--- serwis/settings.py
+++ serwis/settings.py
@@ -38,6 +38,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'mainapp.context_processors.uzytkownik_aplikacji',
             ],
         },
     },
