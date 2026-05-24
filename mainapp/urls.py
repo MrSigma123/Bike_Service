e5e39959e37f6b373940c692e192e64707417a9a
MrSigma123 <karol.czolpinski@gmail.com>
2026-05-24 15:03:59 +0200

    Fix urls blending

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('rowery/', views.rowery, name='rowery'),
     path('rowery/dodaj/', views.dodaj_rower, name='dodaj_rower'),
